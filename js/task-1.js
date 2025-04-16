a43cc5ad950b8603a568401287547b2a869250fb
odoker <odoker@msn.com>
2025-04-16 18:25:26 +0300

    Third revision

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-function makeTransaction (quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return Number.parseInt(content) + 2 * (Number.parseInt(padding) + Number.parseFloat(border));
+    return Number.parseFloat(content) + 2 * (Number.parseFloat(padding) + Number.parseFloat(border));
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
